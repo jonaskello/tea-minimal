90fd28e3323beefd38faee5b33e5dadd336e6b5c
Jonas Kello <jonas.kello@gmail.com>
2022-01-24 13:38:57 +0100

    Runtime render-agnostic

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+import ReactDOM from "react-dom";
 import { Program, run } from "./runtime";
 import { State, Action, init, view, update } from "./root";
 
-const myProgram: Program<State, Action> = { init, view, update };
+const myProgram: Program<State, Action, JSX.Element> = { init, view, update };
 
-run(myProgram, document.getElementById("root")!);
+const render = (jsx: JSX.Element) => ReactDOM.render(jsx, document.getElementById("root")!);
+
+run(myProgram, render);
--- src/runtime.ts
+++ src/runtime.ts
@@ -1,18 +1,18 @@
 import ReactDOM from "react-dom";
 
-export type Program<TState, TAction> = {
+export type Program<TState, TAction, TView> = {
   init: () => TState;
-  view: (state: TState, dispatch: Dispatch<TAction>) => JSX.Element;
+  view: (state: TState, dispatch: Dispatch<TAction>) => TView;
   update: (state: TState, action: TAction) => TState;
 };
 
 export type Dispatch<TAction> = (action: TAction) => void;
 
-export function run(program: Program<any, any>, elem: HTMLElement): void {
+export function run(program: Program<any, any, any>, render: (view: any) => void): void {
   let state = program.init();
   const dispatch = (action: any) => {
     state = program.update(state, action);
-    ReactDOM.render(program.view(state, dispatch), elem);
+    render(program.view(state, dispatch));
   };
-  ReactDOM.render(program.view(state, dispatch), elem);
+  render(program.view(state, dispatch));
 }
