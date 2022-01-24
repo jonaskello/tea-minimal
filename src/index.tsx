e270c70d5cedd4617e458624bca63f6d5698d801
Jonas Kello <jonas.kello@gmail.com>
2022-01-24 13:26:54 +0100

    Rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Program, run } from "./runtime";
-import { State, Action, init, view, update } from "./my-program";
+import { State, Action, init, view, update } from "./root";
 
 const myProgram: Program<State, Action> = { init, view, update };
 
